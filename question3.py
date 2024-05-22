959e7e05deb22195460065c662f810a013e7f725
SuryaPrabha145 <154042556+SuryaPrabha145@users.noreply.github.com>
2024-05-22 10:48:59 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,8 @@
 #write a program to find the factorial of a nummber
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+ number = int(input("Enter a number to calculate its factorial: "))
+print("Factorial of", number, "is", factorial(number))
